c5f9381c2d03ff3daaab49f7ae96f9f6da4fd03b
dwaq <ee.tinkerer@gmail.com>
2017-07-16 16:21:40 -0400

    write actual functionality

--- IR-Closet-Lights.ino
+++ IR-Closet-Lights.ino
@@ -17,6 +17,13 @@
 #include <IRLib_P01_NEC.h> 
 #include <IRLibCombo.h>
 
+// switch attached to door is on pin 12
+#define DOOR    12
+
+// states for button on door
+#define OPEN     0
+#define CLOSED   1
+
 // All on or off
 #define ON      0xFF00FF
 #define OFF     0xFF807F
@@ -37,20 +44,19 @@
 IRsend mySender;
 
 void setup() {
-  Serial.begin(9600);
-  delay(2000); while (!Serial); //delay for Leonardo
-  Serial.println(F("Every time you press a key is a serial monitor we will send."));
+  // switch is pulled up
+  pinMode(DOOR, INPUT_PULLUP);
 }
 
 void loop() {
-  if (Serial.read() != -1) {
-    // send a code every time a character is received from the 
-    // serial port. You could modify this sketch to send when you
-    // push a button connected to an digital input pin.
-    // Substitute values and protocols in the following statement
-    // for device you have available.
-    mySender.send(NEC,ON,0);  // turn on
-    Serial.println(F("Sent signal."));
-  }
+  // start by turning on
+  mySender.send(NEC, ON, 0);
+  // loop until door is closed
+  while (digitalRead(DOOR) == OPEN);
+
+  // turn off lights
+  mySender.send(NEC, OFF, 0);
+  // loop until door is opened again
+  while (digitalRead(DOOR) == CLOSED);
 }
 
